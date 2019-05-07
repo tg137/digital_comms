10fa5dccedade7987f75568130fb266c896bd36c
ej550 <edward.oughton@gmail.com>
2019-05-07 12:24:53 +0100

    remove sshloginfile nodeslist

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -4,6 +4,7 @@ exchanges=`python scripts/network_cluster_input_files.py $1`
 #echo $exchanges
 
 # #seq 1 $count | parallel -n1 --no-notice ./plot_stations.py {}
-parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/scripts/network_preprocess_input_files.sh ::: $exchanges
+#parallel --sshloginfile nodeslist -n1 --no-notice --progress `pwd`/scripts/network_preprocess_input_files.sh ::: $exchanges
+parallel -n1 --no-notice --progress `pwd`/scripts/network_preprocess_input_files.sh ::: $exchanges
 
 echo 'parallel processing complete'
