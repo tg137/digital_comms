47c9b034c6015487ba4d2d114b5ed3afef4bbe5a
ej550 <edward.oughton@gmail.com>
2019-05-07 12:11:34 +0100

    avoid source activate

--- scripts/network_preprocess_input_files.sh
+++ scripts/network_preprocess_input_files.sh
@@ -2,11 +2,11 @@
 . /etc/profile.d/profile.sh
 echo Pre-processing $1: Running on `hostname`
 
-source activate digital_comms
+# source activate digital_comms
 cd /soge-home/projects/mistral/nismod/digital_comms/
 
 #cd /soge-home/projects/mistral/nismod/digital_comms
-mkdir -p /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/$1
-touch /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/$1/log.txt
+mkdir -p /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1
+touch /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
 
-python `pwd`/scripts/network_preprocess_input_files.py $1 &> /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/$1/log.txt
+python `pwd`/scripts/network_preprocess_input_files.py $1 &> /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
