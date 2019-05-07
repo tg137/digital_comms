3cf3423d2b8a201fde278c611ccd777b999c6d1d
ej550 <edward.oughton@gmail.com>
2019-05-07 15:38:07 +0100

    Make directory first if not present

--- scripts/network_preprocess_input_files.sh
+++ scripts/network_preprocess_input_files.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 . /opt/miniconda/etc/profile.d/conda.sh
 conda activate digital_comms
+
+#cd /soge-home/projects/mistral/nismod/digital_comms
+mkdir -p /soge-home/projects/mistral/nismod/digital_comms/data/intermediate/intermediate_exchanges/$1
+touch /soge-home/projects/mistral/nismod/digital_comms/data/intermediate/intermediate_exchanges/$1/log.txt
+
 python $(dirname $0)/network_preprocess_input_files.py $1  'False' &> \
 /soge-home/projects/mistral/nismod/digital_comms/data/intermediate/intermediate_exchanges/$1/log.txt
 
 # # source activate digital_comms
 # cd /soge-home/projects/mistral/nismod/digital_comms/
 
-# #cd /soge-home/projects/mistral/nismod/digital_comms
-# mkdir -p /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1
-# touch /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
+
 
 # python `pwd`/scripts/network_preprocess_input_files.py $1 &> /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
