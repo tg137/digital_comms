b94537ef8e7dcb18165f58ef8a556cff06ae7800
ej550 <edward.oughton@gmail.com>
2019-05-07 12:38:11 +0100

    test updated network_preprocess.sh

--- scripts/network_preprocess_input_files.sh
+++ scripts/network_preprocess_input_files.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-. /etc/profile.d/profile.sh
-echo Pre-processing $1: Running on `hostname`
+. /opt/miniconda/etc/profile.d/conda.sh
+conda activate digital_comms
+python $(dirname $0)/network_preprocess_input_files.py $1
 
-# source activate digital_comms
-cd /soge-home/projects/mistral/nismod/digital_comms/
+# # source activate digital_comms
+# cd /soge-home/projects/mistral/nismod/digital_comms/
 
-#cd /soge-home/projects/mistral/nismod/digital_comms
-mkdir -p /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1
-touch /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
+# #cd /soge-home/projects/mistral/nismod/digital_comms
+# mkdir -p /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1
+# touch /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
 
-python `pwd`/scripts/network_preprocess_input_files.py $1 &> /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
+# python `pwd`/scripts/network_preprocess_input_files.py $1 &> /soge-home/projects/mistral/nismod/digital_comms/data/digital_comms/intermediate/intermediate_exchanges/$1/log.txt
